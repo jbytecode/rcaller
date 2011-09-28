d92971bb15b0b9ca031e7482ac6065196ae323e2
mhsatman <mhsatman@gmail.com>
2011-09-28 12:53:34 +0300

    Enhanced support for Java Objects. Now Rcaller pass a plain java object to R with contents of int,double,float,short,boolean data types and arrays of them.

---
+++ RCaller/src/rcaller/CodeUtils.java
@@ -0,0 +1,122 @@
+package rcaller;
+
+/*
+ * I wrote this code dirty because the methods have similar content.
+ * More automation needed.
+ */
+public class CodeUtils {
+
+  public static void addIntArray(StringBuffer RCode, String name, int[] arr, boolean useEquals) {
+    if (useEquals) {
+      RCode.append(name).append("=").append("c(");
+    } else {
+      RCode.append(name).append("<-").append("c(");
+    }
+    for (int i = 0; i < arr.length; i++) {
+      RCode.append(String.valueOf(arr[i]));
+      if (i < arr.length - 1) {
+        RCode.append(", ");
+      }
+    }
+    if (useEquals) {
+      RCode.append(")");
+    } else {
+      RCode.append(");").append("\n");
+    }
+  }
+
+  public static void addFloatArray(StringBuffer RCode, String name, float[] arr, boolean useEquals) {
+    if (useEquals) {
+      RCode.append(name).append("=").append("c(");
+    } else {
+      RCode.append(name).append("<-").append("c(");
+    }
+    for (int i = 0; i < arr.length; i++) {
+      RCode.append(String.valueOf(arr[i]));
+      if (i < arr.length - 1) {
+        RCode.append(", ");
+      }
+    }
+    if (useEquals) {
+      RCode.append(")");
+    } else {
+      RCode.append(");").append("\n");
+    }
+  }
+
+  public static void addDoubleArray(StringBuffer RCode, String name, double[] arr, boolean useEquals) {
+    if (useEquals) {
+      RCode.append(name).append("=").append("c(");
+    } else {
+      RCode.append(name).append("<-").append("c(");
+    }
+    for (int i = 0; i < arr.length; i++) {
+      RCode.append(String.valueOf(arr[i]));
+      if (i < arr.length - 1) {
+        RCode.append(", ");
+      }
+    }
+    if (useEquals) {
+      RCode.append(")");
+    } else {
+      RCode.append(");").append("\n");
+    }
+  }
+
+  public static void addStringArray(StringBuffer RCode, String name, String[] arr, boolean useEquals) {
+    if (useEquals) {
+      RCode.append(name).append("=").append("c(");
+    } else {
+      RCode.append(name).append("<-").append("c(");
+    }
+    for (int i = 0; i < arr.length; i++) {
+      RCode.append("\"").append(arr[i]).append("\"");
+      if (i < arr.length - 1) {
+        RCode.append(", ");
+      }
+    }
+    if (useEquals) {
+      RCode.append(")");
+    } else {
+      RCode.append(");").append("\n");
+    }
+  }
+
+  public static void addShortArray(StringBuffer RCode, String name, short[] arr, boolean useEquals) {
+    if (useEquals) {
+      RCode.append(name).append("=").append("c(");
+    } else {
+      RCode.append(name).append("<-").append("c(");
+    }
+    for (int i = 0; i < arr.length; i++) {
+      RCode.append(String.valueOf(arr[i]));
+      if (i < arr.length - 1) {
+        RCode.append(", ");
+      }
+    }
+    if (useEquals) {
+      RCode.append(")");
+    } else {
+      RCode.append(");").append("\n");
+    }
+  }
+
+  public static void addLogicalArray(StringBuffer RCode, String name, boolean[] arr, boolean useEquals) {
+    if (useEquals) {
+      RCode.append(name).append("=").append("c(");
+    } else {
+      RCode.append(name).append("<-").append("c(");
+    }
+    for (int i = 0; i < arr.length; i++) {
+      RCode.append(String.valueOf(arr[i]).toUpperCase());
+      if (i < arr.length - 1) {
+        RCode.append(", ");
+      }
+    }
+    if (useEquals) {
+      RCode.append(")");
+    } else {
+      RCode.append(");").append("\n");
+    }
+  }
+}
