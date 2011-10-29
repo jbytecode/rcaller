3ff412674e2332284c48a5df63ea04186428df1c
mhsatman <mhsatman@gmail.com>
2011-10-29 13:13:12 +0300

    Php edition initials

--- RCallerPhp/Globals.php
+++ RCallerPhp/Globals.php
@@ -1,34 +1,31 @@
 <?php
 
 class Globals {
-  
-    public static $cranRepos = "http://cran.r-project.org";
-    
-    /*
-    public static String RScript_Windows = "C:\\Program Files\\R\\R-2.13.0\\bin\\Rscript.exe";
-	public static String RScript_Linux = "/usr/bin/Rscript";
-	public static String Rscript_current;
-	public static String R_Windows = "C:\\Program Files\\R\\R-2.13.0\\bin\\R.exe";
-	public static String R_Linux = "/usr/bin/R";
-	public static String R_current;
-        
-        public static GraphicsTheme theme = new DefaultTheme();
-                
-	public final static String version = "RCaller 2.0";
-	public final static String about = "Author: Mehmet Hakan Satman - mhsatman@yahoo.com";
-	public final static String licence = "LGPL v3.0";
-        
 
-	public static void detect_current_rscript() {
-		if (System.getProperty("os.name").contains("Windows")) {
-			Rscript_current = RScript_Windows;
-			R_current = R_Windows;
-		} else {
-			Rscript_current = RScript_Linux;
-			R_current = R_Linux;
-		}
-	}
-    */
+  public static $cranRepos = "http://cran.r-project.org";
+  public static $RScript_Windows = "C:\\Program Files\\R\\R-2.13.0\\bin\\Rscript.exe";
+  public static $RScript_Linux = "/usr/bin/Rscript";
+  public static $Rscript_current;
+  public static $R_Windows = "C:\\Program Files\\R\\R-2.13.0\\bin\\R.exe";
+  public static $R_Linux = "/usr/bin/R";
+  public static $R_current;
+  
+  //Theme Support did not implemented yet for php version
+  //public static $theme = new DefaultTheme();
+
+  public final static $version = "RCaller 2.0";
+  public final static $about = "Author: Mehmet Hakan Satman - mhsatman@yahoo.com";
+  public final static $licence = "LGPL v3.0";
+
+  public static function detect_current_rscript() {
+    if (strtoupper(substr(PHP_OS, 0, 3)) == "WIN") {
+      Globals::$Rscript_current = Globals::$RScript_Windows;
+      Globals::$R_current = Globals::$R_Windows;
+    } else {
+      Globals::$Rscript_current = Globals::$RScript_Linux;
+      Globals::$R_current = Globals::$R_Linux;
+    }
+  }
 
 }
 
--- RCallerPhp/RCaller.php
+++ RCallerPhp/RCaller.php
@@ -1,6 +1,362 @@
 <?php
+/*
+RCallerPhp, A solution for calling R from Php
+Copyright (C) 2010,2011  Mehmet Hakan Satman
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ 
+  Mehmet Hakan Satman - mhsatman@yahoo.com
+  http://www.mhsatman.com
+  Google code projec: http://code.google.com/p/rcaller/
+
+ *
+ * @author Mehmet Hakan Satman
+ * mhsatman@yahoo.com
+ * http://stdioe.blogspot.com
+ * http://www.mhsatman.com
+ * http://code.google.com/p/rcaller
+ * 
+ */
 
 class RCaller {
+
+  private $RscriptExecutable;
+  private $RExecutable;
+  private $rcode;
+  private $parser;
+  private $process;
+  private $inputStreamToR = null;
+  private $outputStreamToR = null;
+  private $errorStreamToR = null;
+
+  public function getErrorStreamToR() {
+    return $this->errorStreamToR;
+  }
+
+  public function setErrorStreamToR($errorStreamToR) {
+    $this->errorStreamToR = $errorStreamToR;
+  }
+
+  public function getProcess() {
+    return $this->process;
+  }
+
+  public function setProcess($process) {
+    $this->process = $process;
+  }
+
+  public function getInputStreamToR() {
+    return $inputStreamToR;
+  }
+
+  public function setInputStreamToR($inputStreamToR) {
+    $this->inputStreamToR = $inputStreamToR;
+  }
+
+  public function getOutputStreamToR() {
+    return $this->outputStreamToR;
+  }
+
+  public function setOutputStreamToR($outputStreamToR) {
+    $this->outputStreamToR = $outputStreamToR;
+  }
+
+  public function getRExecutable() {
+    return $this->RExecutable;
+  }
+
+  public function setRExecutable($RExecutable) {
+    $this->RExecutable = $RExecutable;
+  }
+
+  public function getCranRepos() {
+    return Globals::$cranRepos;
+  }
+
+  public function setCranRepos($cranRepos) {
+    Globals::$cranRepos =  $cranRepos;
+  }
+
+  public function getParser() {
+    return  $parser;
+  }
+
+  public function setParser($parser) {
+    $this->parser = $parser;
+  }
+
+  public function getRCode() {
+    return $this->rcode;
+  }
+
+  public function setRCode($rcode) {
+    $this->rcode = $rcode;
+  }
+
+  public function getRscriptExecutable() {
+    return $this->RscriptExecutable;
+  }
+
+  public function setRscriptExecutable($RscriptExecutable) {
+    $this->RscriptExecutable = $RscriptExecutable;
+  }
+
+  function __constructor() {
+    $this->rcode = new RCode("");
+    //$this.parser = new ROutputParser();
+    $this->cleanRCode();
+  }
+  
+  public function setGraphicsTheme($theme){
+    Globals::$theme = $theme;
+  }
+
+  public function cleanRCode() {
+    $this->rcode->clear();
+    $this->rcode->addRCode("packageExist<-require(Runiversal)");
+    $this->rcode->addRCode("if(!packageExist){");
+    $this->rcode->addRCode("install.packages(\"Runiversal\", repos=\" " . $this->getCranRepos() ."\")");
+    $this->rcode->addRCode("}\n");
+  }
+
+  /**
+   * @deprecated Use RCode.addRCode instead
+   * @param code 
+   */
+  public function addRCode($code) {
+    $this->rcode->getCode() .= $code."\n";
+  }
+
+  /**
+   * @deprecated Use RCode.addStringArray instead
+   * @param name
+   * @param arr 
+   */
+  public function addStringArray($name, $arr) {
+    CodeUtils::addStringArray($this->rcode->getCode(),  $this->name, $arr, false);
+  }
+
+  /**
+   * @deprecated Use RCode.addDoubleArray
+   * @param name
+   * @param arr 
+   */
+  public function addNumericArray($name, $arr) {
+    CodeUtils::addNumericArray($rcode->getCode(), $name, $arr, false);
+  }
+
+  
+    /**
+   * @deprecated Use RCode.startPlot
+   * @param name
+   * @param arr 
+   */
+  public function startPlot() {
+    return($this->rcode->startPlot());
+  }
+
+    /**
+   * @deprecated Use RCode.endPlot
+   * @param name
+   * @param arr 
+   */
+  public function endPlot() {
+    $rcode->endPlot();
+  }
+
+    /**
+   * @deprecated Use RCode.getPlot
+   * @param name
+   * @param arr 
+   */
+  public function getPlot($f) {
+    return $this->rcode->getPlot($f);
+  }
+
+    /**
+   * @deprecated Use RCode.showPlot
+   * @param name
+   * @param arr 
+   */
+  public function showPlot($f) {
+    $this->rcode->showPlot($f);
+  }
+
+  /*
+  public File createRSourceFile() throws rcaller.exception.RCallerExecutionException {
+    File f = null;
+    BufferedWriter writer = null;
+
+    try {
+      f = File.createTempFile("rcaller", "");
+    } catch (Exception e) {
+      throw new RCallerExecutionException("Can not open a tempopary file for storing the R Code: " + e.toString());
+    }
+
+    try {
+      writer = new BufferedWriter(new FileWriter(f));
+      writer.write(this.rcode.toString());
+      writer.flush();
+    } catch (Exception e) {
+      throw new RCallerExecutionException("Can not write to temporary file for storing the R Code: " + e.toString());
+    } finally {
+      try {
+        writer.close();
+      } catch (Exception einner) {
+      }
+    }
+
+    return (f);
+  }
+
+  public void runOnly() throws rcaller.exception.RCallerExecutionException {
+    if (this.RscriptExecutable == null) {
+      throw new RCallerExecutionException("RscriptExecutable is not defined. Please set this variable to full path of Rscript executable binary file.");
+    }
+    this.rcode.getCode().append("q(").append("\"").append("yes").append("\"").append(")\n");
+    File rSourceFile = createRSourceFile();
+    try {
+      //this Process object is local to this method. Do not use the public one.
+      process = Runtime.getRuntime().exec(RscriptExecutable + " " + rSourceFile.toString());
+      process.waitFor();
+    } catch (Exception e) {
+      throw new RCallerExecutionException("Can not run " + RscriptExecutable + ". Reason: " + e.toString());
+    }
+  }
+
+  public void runAndReturnResultOnline(String var) throws rcaller.exception.RCallerExecutionException {
+    String commandline = null;
+    String result = null;
+    File rSourceFile;
+    final File outputFile;
+
+    if (this.RExecutable == null) {
+      throw new RCallerExecutionException("RExecutable is not defined. Please set this variable to full path of R executable binary file.");
+    }
+
+
+    try {
+      outputFile = File.createTempFile("Routput", "");
+    } catch (Exception e) {
+      throw new RCallerExecutionException("Can not create a tempopary file for storing the R results: " + e.toString());
+    }
+
+    this.rcode.getCode().append("cat(makexml(obj=").append(var).append(", name=\"").append(var).append("\"), file=\"").append(outputFile.toString().replace("\\", "/")).append("\")\n");
+
+    if (outputStreamToR == null || inputStreamToR == null || errorStreamToR == null || process == null) {
+      try {
+        commandline = RExecutable + " --vanilla";
+        process = Runtime.getRuntime().exec(commandline);
+        outputStreamToR = process.getOutputStream();
+        inputStreamToR = process.getInputStream();
+        errorStreamToR = process.getErrorStream();
+      } catch (Exception e) {
+        throw new RCallerExecutionException("Can not run " + RExecutable + ". Reason: " + e.toString());
+      }
+    }
+
+    InputStreamConsumer isConsumer = new InputStreamConsumer(inputStreamToR);
+    InputStreamConsumer errConsumer = new InputStreamConsumer(errorStreamToR);
+    isConsumer.getConsumerThread().start();
+    errConsumer.getConsumerThread().start();
+
+    try {
+      outputStreamToR.write(rcode.toString().getBytes());
+      outputStreamToR.flush();
+    } catch (Exception e) {
+      throw new RCallerExecutionException("Can not send the source code to R file due to: " + e.toString());
+    }
+
+    Thread calcThread = new Thread(new Runnable() {
+
+      @Override
+      public void run() {
+        while (outputFile.length() < 1) {
+          try {
+            Thread.sleep(1);
+          } catch (Exception e) {
+          }
+        }
+      }
+    });
+    calcThread.start();
+    try {
+      calcThread.join();
+    } catch (Exception e) {
+      //Do nothing here
+    }
+
+    isConsumer.setCloseSignal(true);
+    errConsumer.setCloseSignal(true);
+
+    parser.setXMLFile(outputFile);
+
+    try {
+      parser.parse();
+    } catch (Exception e) {
+      throw new RCallerExecutionException("Can not handle R results due to : " + e.toString());
+    }
+  }
+
+  public void runAndReturnResult(String var) throws rcaller.exception.RCallerExecutionException {
+    String commandline = null;
+    String result = null;
+    File rSourceFile, outputFile;
+
+    if (this.RscriptExecutable == null) {
+      throw new RCallerExecutionException("RscriptExecutable is not defined. Please set this variable to full path of Rscript executable binary file.");
+    }
+
+
+    try {
+      outputFile = File.createTempFile("Routput", "");
+    } catch (Exception e) {
+      throw new RCallerExecutionException("Can not create a tempopary file for storing the R results: " + e.toString());
+    }
+
+    rcode.getCode().append("cat(makexml(obj=").append(var).append(", name=\"").append(var).append("\"), file=\"").append(outputFile.toString().replace("\\", "/")).append("\")\n");
+    rSourceFile = createRSourceFile();
+    try {
+      commandline = RscriptExecutable + " " + rSourceFile.toString();
+      //this Process object is local to this method. Do not use the public one.
+      Process process = Runtime.getRuntime().exec(commandline);
+      process.waitFor();
+    } catch (Exception e) {
+      throw new RCallerExecutionException("Can not run " + RscriptExecutable + ". Reason: " + e.toString());
+    }
+
+
+    parser.setXMLFile(outputFile);
+    try {
+      parser.parse();
+    } catch (Exception e) {
+      System.out.println(rcode.toString());
+      throw new RCallerExecutionException("Can not handle R results due to : " + e.toString());
+    }
+  }
+
+  public void R_require(String pkg) {
+    this.rcode.code = this.rcode.getCode().insert(0, "require(" + pkg + ")\n");
+  }
+
+  public void R_source(String sourceFile) {
+    rcode.addRCode("source(\"" + sourceFile + "\")\n");
+  }
+}
+   
+*/
   
 }
 
