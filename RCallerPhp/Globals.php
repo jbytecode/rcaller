243810a2a982cacd1982d052fa9c1e697225b84c
mhsatman <mhsatman@gmail.com>
2011-10-30 15:13:45 +0200

    RCallerPhp is also licenced with LGPL. Tidying up the code

--- RCallerPhp/Globals.php
+++ RCallerPhp/Globals.php
@@ -1,5 +1,30 @@
 <?php
 
+/*
+ *
+  RCallerPhp, A solution for calling R from Php
+  Copyright (C) 2010,2011  Mehmet Hakan Satman
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Mehmet Hakan Satman - mhsatman@yahoo.com
+ * http://www.mhsatman.com
+ * Google code projec: http://code.google.com/p/rcaller/
+ *
+ */
+
 class Globals {
 
   public static $cranRepos = "http://cran.r-project.org";
@@ -9,7 +34,6 @@ class Globals {
   public static $R_Windows = "C:\\Program Files\\R\\R-2.13.0\\bin\\R.exe";
   public static $R_Linux = "/usr/bin/R";
   public static $R_current;
-  
   //Theme Support did not implemented yet for php version
   //public static $theme = new DefaultTheme();
 
--- RCallerPhp/RCode.php
+++ RCallerPhp/RCode.php
@@ -1,5 +1,30 @@
 <?php
 
+/*
+ *
+  RCallerPhp, A solution for calling R from Php
+  Copyright (C) 2010,2011  Mehmet Hakan Satman
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Mehmet Hakan Satman - mhsatman@yahoo.com
+ * http://www.mhsatman.com
+ * Google code projec: http://code.google.com/p/rcaller/
+ *
+ */
+
 include_once ("Globals.php");
 
 class RCode {
@@ -27,10 +52,10 @@ class RCode {
     $this->addRCode("}\n");
   }
 
-  public function prependRCode($code){
-    $this->code = $code . $this->code . "\n"; 
+  public function prependRCode($code) {
+    $this->code = $code . $this->code . "\n";
   }
-  
+
   public function addRCode($code) {
     $this->code .= $code . "\n";
   }
