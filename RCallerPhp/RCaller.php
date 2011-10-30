28c71f58b566b296b2bd575588005a382d69e760
mhsatman <mhsatman@gmail.com>
2011-10-30 14:34:25 +0200

    RCallerPhp is ready for testing

--- RCallerPhp/RCaller.php
+++ RCallerPhp/RCaller.php
@@ -1,22 +1,23 @@
 <?php
+
 /*
-RCallerPhp, A solution for calling R from Php
-Copyright (C) 2010,2011  Mehmet Hakan Satman
+  RCallerPhp, A solution for calling R from Php
+  Copyright (C) 2010,2011  Mehmet Hakan Satman
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
- 
- 
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
   Mehmet Hakan Satman - mhsatman@yahoo.com
   http://www.mhsatman.com
   Google code projec: http://code.google.com/p/rcaller/
@@ -29,6 +30,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * http://code.google.com/p/rcaller
  * 
  */
+
+include_once ("CodeUtils.php");
+include_once ("RCode.php");
+include_once ("ROutputParser.php");
 
 class RCaller {
 
@@ -86,11 +91,11 @@ class RCaller {
   }
 
   public function setCranRepos($cranRepos) {
-    Globals::$cranRepos =  $cranRepos;
+    Globals::$cranRepos = $cranRepos;
   }
 
   public function getParser() {
-    return  $parser;
+    return $this->parser;
   }
 
   public function setParser($parser) {
@@ -113,13 +118,13 @@ class RCaller {
     $this->RscriptExecutable = $RscriptExecutable;
   }
 
-  function __constructor() {
+  function __construct() {
     $this->rcode = new RCode("");
-    //$this.parser = new ROutputParser();
+    $this->parser = new ROutputParser();
     $this->cleanRCode();
   }
-  
-  public function setGraphicsTheme($theme){
+
+  public function setGraphicsTheme($theme) {
     Globals::$theme = $theme;
   }
 
@@ -127,7 +132,7 @@ class RCaller {
     $this->rcode->clear();
     $this->rcode->addRCode("packageExist<-require(Runiversal)");
     $this->rcode->addRCode("if(!packageExist){");
-    $this->rcode->addRCode("install.packages(\"Runiversal\", repos=\" " . $this->getCranRepos() ."\")");
+    $this->rcode->addRCode("install.packages(\"Runiversal\", repos=\" " . $this->getCranRepos() . "\")");
     $this->rcode->addRCode("}\n");
   }
 
@@ -145,7 +150,7 @@ class RCaller {
    * @param arr 
    */
   public function addStringArray($name, $arr) {
-    CodeUtils::addStringArray($this->rcode->getCode(),  $this->name, $arr, false);
+    CodeUtils::addStringArray($this->rcode->getCode(), $this->name, $arr, false);
   }
 
   /**
@@ -157,8 +162,7 @@ class RCaller {
     CodeUtils::addNumericArray($rcode->getCode(), $name, $arr, false);
   }
 
-  
-    /**
+  /**
    * @deprecated Use RCode.startPlot
    * @param name
    * @param arr 
@@ -167,7 +171,7 @@ class RCaller {
     return($this->rcode->startPlot());
   }
 
-    /**
+  /**
    * @deprecated Use RCode.endPlot
    * @param name
    * @param arr 
@@ -176,7 +180,7 @@ class RCaller {
     $rcode->endPlot();
   }
 
-    /**
+  /**
    * @deprecated Use RCode.getPlot
    * @param name
    * @param arr 
@@ -185,7 +189,7 @@ class RCaller {
     return $this->rcode->getPlot($f);
   }
 
-    /**
+  /**
    * @deprecated Use RCode.showPlot
    * @param name
    * @param arr 
@@ -194,52 +198,51 @@ class RCaller {
     $this->rcode->showPlot($f);
   }
 
-  
-  public function createRSourceFile()  {
-    $f = tmpfile();
+  public function createRSourceFile() {
+    $f = CodeUtils::createTempFile("rcallerphp");
+    file_put_contents($f, $this->rcode->toString());
     return ($f);
   }
 
-  
   public function runOnly() {
     if ($this->RscriptExecutable == null) {
       throw new Exception("RscriptExecutable is not defined. Please set this variable to full path of Rscript executable binary file.");
     }
-    $this->rcode->addRCode("q("."\""."yes"."\"".")\n");
+    $this->rcode->addRCode("q(" . "\"" . "yes" . "\"" . ")\n");
     $rSourceFile = $this->createRSourceFile();
     $process = exec(RscriptExecutable . " " . $rSourceFile);
   }
 
   /*
-  public void runAndReturnResultOnline(String var) throws rcaller.exception.RCallerExecutionException {
+    public void runAndReturnResultOnline(String var) throws rcaller.exception.RCallerExecutionException {
     String commandline = null;
     String result = null;
     File rSourceFile;
     final File outputFile;
 
     if (this.RExecutable == null) {
-      throw new RCallerExecutionException("RExecutable is not defined. Please set this variable to full path of R executable binary file.");
+    throw new RCallerExecutionException("RExecutable is not defined. Please set this variable to full path of R executable binary file.");
     }
 
 
     try {
-      outputFile = File.createTempFile("Routput", "");
+    outputFile = File.createTempFile("Routput", "");
     } catch (Exception e) {
-      throw new RCallerExecutionException("Can not create a tempopary file for storing the R results: " + e.toString());
+    throw new RCallerExecutionException("Can not create a tempopary file for storing the R results: " + e.toString());
     }
 
     this.rcode.getCode().append("cat(makexml(obj=").append(var).append(", name=\"").append(var).append("\"), file=\"").append(outputFile.toString().replace("\\", "/")).append("\")\n");
 
     if (outputStreamToR == null || inputStreamToR == null || errorStreamToR == null || process == null) {
-      try {
-        commandline = RExecutable + " --vanilla";
-        process = Runtime.getRuntime().exec(commandline);
-        outputStreamToR = process.getOutputStream();
-        inputStreamToR = process.getInputStream();
-        errorStreamToR = process.getErrorStream();
-      } catch (Exception e) {
-        throw new RCallerExecutionException("Can not run " + RExecutable + ". Reason: " + e.toString());
-      }
+    try {
+    commandline = RExecutable + " --vanilla";
+    process = Runtime.getRuntime().exec(commandline);
+    outputStreamToR = process.getOutputStream();
+    inputStreamToR = process.getInputStream();
+    errorStreamToR = process.getErrorStream();
+    } catch (Exception e) {
+    throw new RCallerExecutionException("Can not run " + RExecutable + ". Reason: " + e.toString());
+    }
     }
 
     InputStreamConsumer isConsumer = new InputStreamConsumer(inputStreamToR);
@@ -248,29 +251,29 @@ class RCaller {
     errConsumer.getConsumerThread().start();
 
     try {
-      outputStreamToR.write(rcode.toString().getBytes());
-      outputStreamToR.flush();
+    outputStreamToR.write(rcode.toString().getBytes());
+    outputStreamToR.flush();
     } catch (Exception e) {
-      throw new RCallerExecutionException("Can not send the source code to R file due to: " + e.toString());
+    throw new RCallerExecutionException("Can not send the source code to R file due to: " + e.toString());
     }
 
     Thread calcThread = new Thread(new Runnable() {
 
-      @Override
-      public void run() {
-        while (outputFile.length() < 1) {
-          try {
-            Thread.sleep(1);
-          } catch (Exception e) {
-          }
-        }
-      }
+    @Override
+    public void run() {
+    while (outputFile.length() < 1) {
+    try {
+    Thread.sleep(1);
+    } catch (Exception e) {
+    }
+    }
+    }
     });
     calcThread.start();
     try {
-      calcThread.join();
+    calcThread.join();
     } catch (Exception e) {
-      //Do nothing here
+    //Do nothing here
     }
 
     isConsumer.setCloseSignal(true);
@@ -279,14 +282,14 @@ class RCaller {
     parser.setXMLFile(outputFile);
 
     try {
-      parser.parse();
+    parser.parse();
     } catch (Exception e) {
-      throw new RCallerExecutionException("Can not handle R results due to : " + e.toString());
+    throw new RCallerExecutionException("Can not handle R results due to : " + e.toString());
     }
-  }
-*/
+    }
+   */
 
-  public function runAndReturnResult($var)  {
+  public function runAndReturnResult($var) {
     $commandline = null;
     $result = null;
     $rSourceFile = null;
@@ -296,16 +299,16 @@ class RCaller {
       throw new Exception("RscriptExecutable is not defined. Please set this variable to full path of Rscript executable binary file.");
     }
 
-    $outputFile = tmpfile();
-    
-    $this->rcode->addRCode("cat(makexml(obj=".$var.", name=\"".$var."\"), file=\"".$outputFile."\")\n");
+    $outputFile = CodeUtils::createTempFile("RCallerPhpOutput");
+
+    $this->rcode->addRCode("cat(makexml(obj=" . $var . ", name=\"" . $var . "\"), file=\"" . $outputFile . "\")\n");
     $rSourceFile = $this->createRSourceFile();
     try {
       $commandline = $this->RscriptExecutable . " " . $rSourceFile;
       //this Process object is local to this method. Do not use the public one.
       $process = exec($commandline);
     } catch (Exception $e) {
-      throw new Exception("Can not run " .  $this->RscriptExecutable . ". Reason: " + $e);
+      throw new Exception("Can not run " . $this->RscriptExecutable . ". Reason: " + $e);
     }
 
 
@@ -319,16 +322,14 @@ class RCaller {
   }
 
   public function R_require($pkg) {
-    $this->rcode->prependRCode ("require(" + pkg + ")");
+    $this->rcode->prependRCode("require(" + pkg + ")");
   }
 
   public function R_source($sourceFile) {
     $this->rcode->addRCode("source(\"" . $sourceFile . "\")");
   }
-  
+
 }
 
-
 $r = new RCaller();
-
 ?>
--- RCallerPhp/test/RCodeTest.php
+++ RCallerPhp/test/RCodeTest.php
@@ -3,8 +3,9 @@
 
 require_once("../RCode.php");
 require_once("../RCaller.php");
+require_once("simpletest.php");
 
-function TestRCode(){
+function TestRCode() {
   $rcode = new RCode("");
   $rcode->clear();
   $rcode->addRCode("a<-1:10");
@@ -12,15 +13,22 @@ function TestRCode(){
   print $predicted;
 }
 
-
-function TestRCaller1(){
+function TestRCaller1() {
   $rcaller = new RCaller();
+  $rcaller->setRscriptExecutable("/usr/bin/Rscript");
   $rcode = new RCode("");
   $rcode->clear();
-  $rcode->addRCode("x<-1:10");
+  $rcode->addRCode("mylist <- list(x=1:3, y=c(7,8,9))");
+
   $rcaller->setRCode($rcode);
-  $rcaller->runAndReturnResult("x");
-  
+  $rcaller->runAndReturnResult("mylist");
+
+  $x = $rcaller->getParser()->getAsStringArray("x");
+  $y = $rcaller->getParser()->getAsStringArray("y");
+
+  test("getting x", $x, array(1, 2, 3));
+  test("getting y", $y, array(7, 8, 9));
 }
 
+TestRCaller1();
 ?>
