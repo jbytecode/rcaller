749f23c9b82a4e1a47b50d17d0cf4d8c47e39215
mhsatman <mhsatman@gmail.com>
2012-04-13 16:39:33 +0300

    Exception handling

--- RCaller/src/main/java/rcaller/CodeUtils.java
+++ RCaller/src/main/java/rcaller/CodeUtils.java
@@ -149,4 +149,6 @@ public class CodeUtils {
       RCode.append(";\n");
     }
   }
+  
+  
 }
---
+++ RCaller/src/main/java/rcaller/exception/RCallerXMLParseException.java
@@ -0,0 +1,14 @@
+package rcaller.exception;
+
+public class RCallerXMLParseException extends RCallerParseException {
+
+	public RCallerXMLParseException() {
+		super();
+	}
+
+	public RCallerXMLParseException(String msg) {
+		super(msg);
+	}
+
+	
+}
