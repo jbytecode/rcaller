eda0bc4c9c16442f81a0cd2016489cfa97baccad
mhsatman <mhsatman@gmail.com>
2014-04-13 11:45:23 +0300

    XML files are now shorter due to tag names. New test added for larger data

--- RCaller/src/main/java/rpackage/Runiversal/R/runiversal_old.r
+++ RCaller/src/main/java/rpackage/Runiversal/R/runiversal_old.r
@@ -16,6 +16,57 @@ cleanNames<-function(names){
 	return(cleanNames);
 }
 
+makevectorjava<-function(code,objt,name=""){
+	javacode<-code;
+		obj<-objt;
+		if(is.matrix(obj)) obj<-as.vector(obj);
+		if(typeof(obj)=="language") obj<-toString(obj);
+        	if(typeof(obj)=="logical") obj<-as.character(obj);
+
+		if(is.vector(obj) && is.numeric(obj)){
+                        javacode<-concat(javacode,"double[] ");
+                        if(name==""){
+				javacode<-concat(javacode,cleanNames(deparse(substitute(obj))));
+			}else{
+				javacode<-concat(javacode,name);
+			}
+                        javacode<-concat(javacode,"= new double[] {");
+                        javacode<-concat(javacode,obj);
+                        javacode<-concat(javacode,"};\n");
+                }
+                if(is.vector(obj) && is.character(obj)){
+                        javacode<-concat(javacode,"String[] ");
+	                if(name==""){
+				javacode<-concat(javacode,cleanNames(deparse(substitute(obj))));
+			}else{
+				javacode<-concat(javacode,name);
+			}
+                        javacode<-concat(javacode,"= new String[] {");
+                        for (elem in 1:length(obj)){
+                                javacode<-paste(javacode,"\"",toString(obj[elem]),"\"",sep="");
+                                if (elem!=length(obj)) javacode<-paste(javacode,",",sep="");
+
+                        }
+                        javacode<-concat(javacode,"};\n");
+                }
+	return(javacode);
+}
+
+makejava<-function(obj,name=""){
+	javacode<-"";
+	if(!is.list(obj)){
+        	javacode<-makevectorjava(javacode,obj,name);
+        }
+	if (is.list(obj)){
+		objnames<-names(obj);
+		for (i in 1:length(obj)){
+			javacode<-makevectorjava(javacode,obj[[i]],cleanNames(objnames[[i]]));
+		}
+	}
+return(javacode);
+}
+
+
 makevectorxml<-function(code,objt,name=""){
         xmlcode<-code;
 	if(name==""){
@@ -30,18 +81,18 @@ makevectorxml<-function(code,objt,name=""){
                 if(is.vector(obj) && is.numeric(obj)){
                         xmlcode<-paste(xmlcode,"<variable name=\"",varname,"\" type=\"numeric\">\n",sep="");
 			for (i in obj){
-                        	xmlcode<-paste(xmlcode,"<v>",sep="");
+                        	xmlcode<-paste(xmlcode,"\t<value>",sep="");
 				xmlcode<-paste(xmlcode,toString(i),sep="");
-	                        xmlcode<-paste(xmlcode,"</v>",sep="");
+	                        xmlcode<-paste(xmlcode,"</value>\n",sep="");
 			}
                         xmlcode<-paste(xmlcode,"</variable>\n",sep="");
                 }
                 if(is.vector(obj) && is.character(obj)){
                         xmlcode<-paste(xmlcode,"<variable name=\"",varname,"\" type=\"character\">\n",sep="");
                         for (i in obj){
-                                xmlcode<-paste(xmlcode,"<v>",sep="");
+                                xmlcode<-paste(xmlcode,"\t<value>",sep="");
                                 xmlcode<-paste(xmlcode,toString(i),sep="");
-                                xmlcode<-paste(xmlcode,"</v>",sep="");
+                                xmlcode<-paste(xmlcode,"</value>\n",sep="");
                         }
 			xmlcode<-paste(xmlcode,"</variable>\n");
                 }
--- RCaller/src/test/java/rcaller/LargeDataTest.java
+++ RCaller/src/test/java/rcaller/LargeDataTest.java
@@ -12,10 +12,11 @@ import org.junit.Test;
 
 public class LargeDataTest {
 
-    double delta = 1/100000;
     
+   
     @Test
-    public void testBigData500(){
+    public void testBigData_1000_10(){
+        double delta = 1/100000;
         double[][] data = new double[1000][10];
         for (int i=0;i<data.length;i++){
             for (int j=0;j<data[0].length;j++){
@@ -37,8 +38,26 @@ public class LargeDataTest {
         
         Assert.assertEquals(result[0], 10.0, delta);
         Assert.assertEquals(result[1], 10.0, delta);
+    }
+    
+    @Test
+    public void testReturnsBigData (){
+        double delta = 0.5;
+        RCaller caller = new RCaller();
+        Globals.detect_current_rscript();
+        caller.setRscriptExecutable(Globals.Rscript_current);
         
+        RCode code = new RCode();
+        code.addRCode("s <- rnorm(1024 * 2)");
+        code.addRCode("m <- mean(s)");
+        code.addRCode("result <- list(arr=s, mean=m)");
+        caller.setRCode(code);
+        caller.runAndReturnResult("result");
         
-
+        double m = caller.getParser().getAsDoubleArray("mean")[0];
+        int len = caller.getParser().getAsDoubleArray("arr").length;
+        
+        Assert.assertEquals(0.0, m, delta);
+        Assert.assertEquals((long)len, 2048l);
     }
 }
