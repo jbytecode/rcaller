9eee7e639e205a426339f524797a23a5f02ae57f
mhsatman <mhsatman@gmail.com>
2014-04-13 19:43:29 +0300

    A test returns boolean added from net

--- RCaller/src/main/java/rcaller/RCode.java
+++ RCaller/src/main/java/rcaller/RCode.java
@@ -32,7 +32,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import javax.swing.ImageIcon;
 import rcaller.exception.ExecutionException;
 
