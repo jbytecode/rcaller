3eeee37b938fd1e4c8138ea64ae4ae3e13bbb5ed
mhsatman <mhsatman@gmail.com>
2014-04-14 14:04:47 +0300

    New test file added for chisquare test on the web (stackoverflow)

--- RCaller/src/test/java/rcaller/TestBugFixesStackOverflow.java
+++ RCaller/src/test/java/rcaller/TestBugFixesStackOverflow.java
@@ -9,9 +9,10 @@ public class TestBugFixesStackOverflow {
     public void testStack1() {
         RCaller caller = new RCaller();
         RCode code = new RCode();
-        caller.setRscriptExecutable("/usr/bin/Rscript");
+        Globals.detect_current_rscript();
+        caller.setRscriptExecutable(Globals.Rscript_current);
         caller.cleanRCode();
-        
+
         String x = "is.installed <- function(mypkg){ \n"
                 + "is.element(mypkg, installed.packages()[,1])\n"
                 + "}\n"
@@ -26,5 +27,31 @@ public class TestBugFixesStackOverflow {
         caller.runAndReturnResult("result");
         boolean result = caller.getParser().getAsLogicalArray("result")[0];
         Assert.assertFalse(result);
+    }
+
+    @Test
+    public void testStack2() {
+        double[][] data = new double[][]{
+            {197.136, 124.32, 63.492, 59.052},
+            {124.32, 78.4, 40.04, 37.24},
+            {63.492, 40.04, 20.449, 19.019},
+            {59.052, 37.24, 19.019, 17.689}
+        };
+        RCaller caller = new RCaller();
+        Globals.detect_current_rscript();
+        caller.setRscriptExecutable(Globals.Rscript_current);
+        RCode code = new RCode();
+
+        code.addDoubleMatrix("mydata", data);
+        code.addRCode("result <- chisq.test(mydata)");
+        code.addRCode("mylist <- list(pval = result$p.value, df=result$parameter)");
+
+        caller.setRCode(code);
+        caller.runAndReturnResult("mylist");
+
+        double pvalue = caller.getParser().getAsDoubleArray("pval")[0];
+        double df = caller.getParser().getAsDoubleArray("df")[0];
+        System.out.println("Pvalue is : " + pvalue);
+        System.out.println("Df is : " + df);
     }
 }
