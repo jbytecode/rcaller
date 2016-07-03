d1e693dc0ddc82f7023d5f001a356f3b019ab693
jbytecode <mhsatman@gmail.com>
2016-07-03 15:46:20 +0300

    Java side matrix serialization in R format

--- RCaller/src/main/java/com/github/rcaller/io/RSerializer.java
+++ RCaller/src/main/java/com/github/rcaller/io/RSerializer.java
@@ -23,7 +23,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * Please visit the blog page with rcaller label:
  * http://stdioe.blogspot.com.tr/search/label/rcaller
  */
-
 package com.github.rcaller.io;
 
 import java.io.ByteArrayOutputStream;
@@ -32,37 +31,71 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 
-
-
-
 public class RSerializer {
 
     ByteArrayOutputStream byteOut;
     DataOutputStream dataOut;
-    
-    public RSerializer(){
+
+    public RSerializer() {
         byteOut = new ByteArrayOutputStream();
         dataOut = new DataOutputStream(byteOut);
     }
-    
-    public void writeVector(double[] v) throws IOException{
-        dataOut.write("A\n".getBytes());            /* version */
-        dataOut.write("2\n".getBytes());
-        dataOut.write("197214\n".getBytes());
-        dataOut.write("131840\n".getBytes());
-        dataOut.write("14\n".getBytes());           /* Vector Type */
-        dataOut.write( (String.valueOf(v.length) + "\n").getBytes() );
-        for (int i=0;i<v.length;i++){
-            dataOut.write(String.valueOf(v[i]).getBytes());
-            dataOut.write("\n".getBytes());
+
+    public void writeVector(double[] v) throws IOException {
+        dataOut.write(getBytes("A"));
+        /* version */
+        dataOut.write(getBytes("2"));
+        dataOut.write(getBytes("197214"));
+        dataOut.write(getBytes("131840"));
+        dataOut.write(getBytes("14"));
+        /* Vector Type */
+        dataOut.write(getBytes(v.length));
+        for (int i = 0; i < v.length; i++) {
+            dataOut.write(getBytes(v[i]));
         }
         dataOut.flush();
         byteOut.flush();
         dataOut.close();
         byteOut.close();
     }
-    
-    public void save(String filename) throws IOException{
+
+    public void writeDoubleMatrix(double[][] d) throws IOException {
+        dataOut.write(getBytes("A"));
+        dataOut.write(getBytes("2"));
+        dataOut.write(getBytes("197214"));
+        dataOut.write(getBytes("131840"));
+        dataOut.write(getBytes("525"));
+        dataOut.write(getBytes(d.length * d[0].length));
+        for (int i = 0; i < d.length; i++) {
+            for (int j = 0; j < d[0].length; j++) {
+                dataOut.write(getBytes(String.valueOf(d[i][j])));
+            }
+        }
+        dataOut.write(getBytes("1026"));
+        dataOut.write(getBytes("1"));
+        dataOut.write(getBytes("262153"));
+        dataOut.write(getBytes("3"));
+        dataOut.write(getBytes("dim"));
+        dataOut.write(getBytes("13"));
+        dataOut.write(getBytes("2"));
+        dataOut.write(getBytes(d.length));
+        dataOut.write(getBytes(d[0].length));
+        dataOut.write(getBytes("254"));
+        dataOut.flush();
+        byteOut.flush();
+        dataOut.close();
+        byteOut.close();
+    }
+
+    byte[] getBytes(String s) {
+        return ((s + "\n").getBytes());
+    }
+
+    byte[] getBytes(double d) {
+        return ((String.valueOf(d) + "\n").getBytes());
+    }
+
+    public void save(String filename) throws IOException {
         FileWriter writer = new FileWriter(filename);
         writer.write(byteOut.toString());
         writer.close();
--- RCaller/src/test/java/com/github/rcaller/io/RSerializerTest.java
+++ RCaller/src/test/java/com/github/rcaller/io/RSerializerTest.java
@@ -1,4 +1,3 @@
-
 package com.github.rcaller.io;
 
 import com.github.rcaller.TempFileService;
@@ -13,32 +12,66 @@ import static org.junit.Assert.assertEquals;
 public class RSerializerTest {
 
     double delta = 1 / 100000;
-    
+
     @Test
-    public void WriteVectorTest() throws IOException{
+    public void WriteVectorTest() throws IOException {
         RSerializer rs = new RSerializer();
-        double[] v = new double[]{1.0,2.0,3.0};
+        double[] v = new double[]{1.0, 2.0, 3.0};
         rs.writeVector(v);
-        
+
         TempFileService tmpServ = new TempFileService();
         File tmpFile = tmpServ.createTempFile("RCaller", "Test");
-        
+
         rs.save(tmpFile.getCanonicalPath());
-        
+
         RCaller caller = RCaller.create();
         RCode code = RCode.create();
-        
-        code.addRCode("conn <- file(\"" +  tmpFile.getCanonicalPath()  + "\" , \"r\")");
+
+        code.addRCode("conn <- file(\"" + tmpFile.getCanonicalPath() + "\" , \"r\")");
         code.addRCode("x <- unserialize(conn)");
         caller.setRCode(code);
-        
+
         caller.runAndReturnResult("x");
-        
+
         double[] x = caller.getParser().getAsDoubleArray("x");
         assertEquals(1.0, x[0], delta);
         assertEquals(2.0, x[1], delta);
         assertEquals(3.0, x[2], delta);
     }
-    
-    
+
+    @Test
+    public void WriteMatrixTest() throws IOException {
+        RSerializer rs = new RSerializer();
+        double[][] v = new double[][]{
+            {1.0, 2.0},
+            {3.0, 4.0},
+            {5.0, 6.0}
+        };
+
+        rs.writeDoubleMatrix(v);
+
+        TempFileService tmpServ = new TempFileService();
+        File tmpFile = tmpServ.createTempFile("RCaller", "Test");
+
+        rs.save(tmpFile.getCanonicalPath());
+
+        RCaller caller = RCaller.create();
+        RCode code = RCode.create();
+
+        code.addRCode("conn <- file(\"" + tmpFile.getCanonicalPath() + "\" , \"r\")");
+        code.addRCode("x <- unserialize(conn)");
+        caller.setRCode(code);
+
+        caller.runAndReturnResult("x");
+
+        double[][] x = caller.getParser().getAsDoubleMatrix("x");
+        assertEquals(1.0, x[0][0], delta);
+        assertEquals(2.0, x[0][1], delta);
+        assertEquals(3.0, x[1][0], delta);
+        assertEquals(4.0, x[1][1], delta);
+        assertEquals(5.0, x[2][0], delta);
+        assertEquals(6.0, x[2][1], delta);
+
+    }
+
 }
